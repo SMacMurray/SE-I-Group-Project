29c6a2cabb62865de74b3dfa7c1f456c7fd677fd
AutoAutoing <autumnsinthewoods@gmail.com>
2026-03-22 14:05:51 -0500

    Implemented Search Available Room

---
+++ SE-I-Group-Project-j-branch-V2.23/src/main/java/BookingPage2.java
@@ -0,0 +1,255 @@
+import javax.swing.*;
+import javax.swing.table.*; // Includes DefaulTableModel
+
+import net.coderazzi.filters.OrFilter;
+import net.coderazzi.filters.gui.AutoChoices;
+import net.coderazzi.filters.gui.LooseParserModel;
+import net.coderazzi.filters.gui.TableFilterHeader;
+
+import java.awt.*; // Includes BorderLayout.*,
+import java.io.*; // Includes File
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import java.util.Scanner;
+import java.util.Calendar;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class BookingPage2 extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private JPanel mainPane;
+	private JPanel homePane;
+	private JButton addRoomButton;
+	private JScrollPane scrollPane;
+	private JPanel alignScrollPane;
+	private TableRowSorter<DefaultTableModel> sorter;
+	private JTable table;
+	private JPanel descriptionPane;
+	private JTextField startDateBox;
+	private JTextField endDateBox;
+	private JTextField guestsBox;
+	
+	private String[] columnNames = {
+			"Room Number",
+			"Beds",
+			"Max Number of Guests",
+			"Base Daily Rate",
+			"Smoking Status",
+			"Bed Sizes",
+			"Quality Level",
+			"Room Size"
+	};
+	
+	public Object[][] initArray() {
+		List<List<Object>> csvData = new ArrayList<>();
+		
+        try (Scanner scanner = new Scanner(new File("reserve.csv")) ) {
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+            	String[] spl = scanner.nextLine().split("\t");
+            	
+            	for( int i = spl.length - 8; i > 0; --i) {
+            		spl[5] = spl[5] + "," + spl[5 + i];
+            		System.out.println("rich");
+            	}
+            	csvData.add(new ArrayList<>(Arrays.asList(
+            			Integer.parseInt(spl[0]),
+            			Integer.parseInt(spl[1]),
+            			Integer.parseInt(spl[2]),
+            			Double.parseDouble(spl[3]),
+            			spl[4],
+            			spl[5].replaceAll("\"", ""),
+            			spl[spl.length - 2],
+            			spl[spl.length - 1]
+            			
+            	)));
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        
+        return csvData.stream()
+                .map(l -> l.stream().toArray(Object[]::new))
+                .toArray(Object[][]::new);
+    }
+	
+	public Object[][] formatRoomsAsArray(List<Room> rooms) {
+		Object[][] roomsArray = new Object[rooms.size()][8];
+		
+		int i = 0;
+		for (Room r : rooms) {
+			roomsArray[i][0] = r.number; 
+			roomsArray[i][1] = r.beds;
+			roomsArray[i][2] = r.maxOccupancy;
+			roomsArray[i][3] = r.baseDailyRate;
+			roomsArray[i][4] = r.smokingStatus ? "Permitted" : "Prohibited";
+			for (int j = 0; j < r.bedTypes.size(); ++j) {				
+				if (r.bedTypes.size() - j == 1) {
+					roomsArray[i][5] += r.bedTypes.get(j).toString();
+				}
+				else {
+					roomsArray[i][5] += r.bedTypes.get(j).name() + ", ";
+				}
+			}
+			// The 'null' comes from the array I made. Not from parsing the files incorrectly.
+			roomsArray[i][5] = ((String)roomsArray[i][5]).replaceAll("null", "");
+			roomsArray[i][6] = r.qualityLevel.toString();
+			roomsArray[i][7] = r.roomSize.toString();
+			++i;
+		}
+		return roomsArray;
+	}
+	public BookingPage2(List<Room> rooms) {
+		Object[][] array = initArray();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize( Main.WINDOW_W + 500, Main.WINDOW_H + 50);
+		
+		mainPane = new JPanel();
+		setContentPane(mainPane);
+		mainPane.setLayout(new BorderLayout());
+		
+		descriptionPane = new JPanel();
+		descriptionPane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		descriptionPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 50, 10)); // Setting padding, positioning the content inside this pane
+		
+		
+		final Class<?>[] columnClass = new Class[] {
+                Integer.class, Integer.class, Integer.class, Double.class, String.class,
+                String.class, String.class, String.class
+        };
+		DefaultTableModel model = new DefaultTableModel(formatRoomsAsArray(rooms), columnNames) {
+			@Override
+            public boolean isCellEditable(int row, int column)
+            {
+                return false;
+            }
+            @Override
+            public Class<?> getColumnClass(int columnIndex)
+            {
+                return columnClass[columnIndex];
+            }
+		};
+		sorter = new TableRowSorter<DefaultTableModel>(model);
+		table = new JTable(model);
+		table.setRowSorter(sorter);
+		TableFilterHeader filterHeader = new TableFilterHeader(table, new LooseParserModel(), AutoChoices.ENABLED);
+		filterHeader.getParserModel().setIgnoreCase(true);
+		table.setPreferredScrollableViewportSize(new Dimension(750, 300));
+		table.setFillsViewportHeight(true);
+		scrollPane = new JScrollPane(table);
+		
+		alignScrollPane = new JPanel();
+		alignScrollPane.setLayout(new FlowLayout());
+		alignScrollPane.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		alignScrollPane.add(scrollPane);
+		
+		JButton homeButton = new JButton(Main.HOME_TEXT);
+        homeButton.setBounds(0, 0, 140, 22);
+        homeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                HomePage newFrame = new HomePage(); //Opening the second JFrame
+                dispose(); //Disposing the First JFrame
+            }
+        });
+        JPanel alignHomePane = new JPanel();
+        alignHomePane.setLayout(new FlowLayout(FlowLayout.LEFT));
+        alignHomePane.add(homeButton);
+        mainPane.add(alignHomePane, BorderLayout.PAGE_START);
+		
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.LINE_START;
+        startDateBox = new JTextField(16);
+        JPanel startDatePanel = new JPanel(new FlowLayout());
+        startDatePanel.add(new JLabel("Start Date: "));
+        startDatePanel.add(startDateBox);
+        descriptionPane.add(startDatePanel, c);
+        c.gridx = 0;
+        c.gridy = 1;
+        endDateBox = new JTextField(16);
+        JPanel endDatePanel = new JPanel(new FlowLayout());
+        endDatePanel.add(new JLabel("End Date: "));
+        endDatePanel.add(endDateBox);
+        descriptionPane.add(endDatePanel, c);
+        c.gridx = 0;
+        c.gridy = 2;
+        guestsBox = new JTextField(16);
+        JPanel guestsPanel = new JPanel(new FlowLayout());
+        guestsPanel.add(new JLabel("Guest Amount: "));
+        guestsPanel.add(guestsBox);
+        descriptionPane.add(guestsPanel, c);
+        
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        addRoomButton = new JButton("Add Room");
+        addRoomButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		// Adding this is only for Iteration 2 / Project Deliverable 9 purposes
+        		int y = table.getSelectedRow();
+        		
+        		if (y < 0) {
+        			JOptionPane.showMessageDialog(null, "You have not selected a row!");
+        		}
+        		else {
+        			int roomNumber = (int)table.getValueAt(y, 0);
+        			
+	        		if (GlobalVariables.rs.getRoom(roomNumber) != null) {
+	        			//JOptionPane.showMessageDialog(null, "This room is already reserved!");
+	        		}
+	        		else {
+	        			List<Room.BedType> bedTypes = new ArrayList<>();
+	        			String str = ((String)table.getValueAt(y, 5)).replaceAll(" ", "");
+	        			String[] spl = str.split(",");
+	        			for (String s : spl) {
+	        				bedTypes.add(Room.BedType.valueOf(s));
+	        			}
+	        			boolean smokingStatus = ( ((String)table.getValueAt(y, 4) == "Permitted") ? false : true);
+	        			GlobalVariables.rs.createRoom((int)table.getValueAt(y, 0), (int)table.getValueAt(y, 1), (int)table.getValueAt(y, 2),
+	        					(double)table.getValueAt(y, 3), smokingStatus, bedTypes,
+	        					Room.QualityLevel.valueOf((String)table.getValueAt(y, 6)), Room.RoomSize.valueOf((String)table.getValueAt(y, 7))
+	        					);
+	        			System.out.println(GlobalVariables.rs.getRoom((int)table.getValueAt(y, 0)).number );
+	        		}
+        		}
+        		
+        		try {
+        			Calendar startDate;
+            		Calendar endDate;
+        			startDate = Calendar.getInstance();
+            		startDate.setTime(formatter.parse(startDateBox.getText()));
+            		endDate = Calendar.getInstance();
+            		endDate.setTime(formatter.parse(endDateBox.getText()));
+            		
+            		Room room = GlobalVariables.rs.getRoom((int)table.getValueAt(y, 0));
+            		int guests = Integer.parseInt(guestsBox.getText());
+            		GlobalVariables.reS.reserveRoom(room, startDate, endDate, guests);
+        		}
+        		catch(ParseException exp) {
+        			JOptionPane.showMessageDialog(null, "Invalid Date Format or Number");
+        		}
+        		//reS.reserveRoom(GlobalVariables.rs.getRoom((int)table.getValueAt(y, 0)), )
+        		
+        	}
+        });
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 3;
+        c.anchor = GridBagConstraints.CENTER;
+        descriptionPane.add(addRoomButton, c);
+        // Add an Alternative Case where the User can't register for a room they already registered for.
+		
+		mainPane.add(alignScrollPane, BorderLayout.LINE_START);
+		//descriptionPane.setSize(new Dimension(100, table.getHeight() - 100));
+		mainPane.add(descriptionPane, BorderLayout.CENTER);
+		
+		setVisible(true);
+	}
+}
