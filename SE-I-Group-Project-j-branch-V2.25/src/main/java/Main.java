d80e25a86b31205aee0cba6a17c322abc75b8e89
AutoAutoing <autumnsinthewoods@gmail.com>
2026-03-23 16:16:31 -0500

    Merged Joel and Scott's use cases
    Joel: Search Room and Reserve Room
    Scott: Add User and Sign In

---
+++ SE-I-Group-Project-j-branch-V2.25/src/main/java/Main.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+// Welcome to my big ball of mud design for making JFrames.
+
+public class Main extends JFrame {
+	//static Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    static int WINDOW_W = 640;
+    static int WINDOW_H = 400;
+    static String APP_TITLE = "Stop & Shop: the illustrious hotel/luxury shopping experience, for all your stop & shopping needs!";
+    static String HOME_TEXT = "Store Logo Here";
+
+ // Use this to indicate what user is currently logged in, or null for logged out.
+    static User SessionAccount = null;
+    
+    public static void initRooms() {
+    	LoadCSV.loadRooms(); // Cant do this outside a function.
+    	
+    }
+
+    public static void main(String[] args) {
+    	initRooms();
+    	
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    HomePage frame = new HomePage();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
