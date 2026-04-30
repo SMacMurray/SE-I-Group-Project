2c3ff44a8d07e48d0dd83763b8e4c02d874177cc
AxolotlGav <gavinxpena@gmail.com>
2026-04-29 19:32:43 -0500

    Updated Product to have ID and added constructors

--- src/main/java/stay_and_shop_system/store/Product.java
+++ src/main/java/stay_and_shop_system/store/Product.java
@@ -3,27 +3,39 @@ package stay_and_shop_system.store;
 import java.util.*;
 
 public class Product {
+	private int id;
 	private double price;
 	private String description;
 	private String name;
-	
+
+    public Product(int id, double price, String description, String name) {
+        this.id = id;
+        this.price = price;
+        this.description = description;
+        this.name = name;
+    }
+
+	public Product() {}
+
+	public int getId() { return id; }
 	public double getPrice() { return price; }
 	public String getDescription() { return description; }
 	public String getName() { return name; }
-	
+
+	public void setId(int i) { id = i; }
 	public void setPrice(double p) {price = p;}
 	public void setDescription(String d) { description = d; }
 	public void setName(String n) { name = n; }
-	
+
 	@Override
 	public boolean equals(Object o) {
-		if (o == this) { return true; }
-		if (!(o instanceof Product)) { return false; }
-		Product p = (Product)o;
-		return (price == p.price) && (description.equals(p.description)) && (name.equals(p.name));
+		if (o == null || getClass() != o.getClass()) return false;
+		Product product = (Product) o;
+		return id == product.id && Double.compare(price, product.price) == 0 && Objects.equals(description, product.description) && Objects.equals(name, product.name);
 	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(price, description, name);
+		return Objects.hash(id, price, description, name);
 	}
 }
